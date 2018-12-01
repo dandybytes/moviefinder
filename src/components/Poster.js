cbe1d6a28ffda2cfca79c0a7d049176e5e4c3559
dandybytes <dandybytes@gmx.com>
2018-12-01 06:07:10 +0200

    further tweaked Poster component (content & styling)

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,39 +1,54 @@
 import React, { Component } from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import { url_img_backdrop } from "../tools/url";
 import "./Poster.css";
 
 class Poster extends Component {
   state = {
+    isLoading: true,
+    title: "",
+    description: "",
+    image: ""
+  };
 
-  }
-  render() {
-    console.log("poster props");
-    console.log(this.props);
-    let backdrop;
-    if (this.props.movies.movies) {
-      backdrop = this.props.movies.movies[2].backdrop_path;
+  componentDidUpdate() {
+    if (this.props.movies.movies && this.state.isLoading) {
+      const randIndex = Math.floor(
+        Math.random() * this.props.movies.movies.length
+      );
+      const movie = this.props.movies.movies[randIndex];
+      const backdrop = movie.backdrop_path;
+      const image = `${url_img_backdrop}${backdrop}`;
+      const title = movie.title;
+      const description = movie.overview;
+      this.setState({ isLoading: false, image, title, description });
     }
+  }
 
+  render() {
     return (
       <section className="poster">
+        <div className="spinner-large" />
         <div
           className="poster-image"
-          style={backdrop && {backgroundImage: `url("${url_img_backdrop}${backdrop}")`}}
+          style={{
+            backgroundImage: `linear-gradient(var(--color-1), transparent 20%), url("${
+              this.state.image
+            }")`
+          }}
         />
         <div className="poster-text">
-          <h1 className="poster-title">{"Movie Title"}</h1>
-          <p className="poster-description">
-            {
-              "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa perferendis neque doloremque. Ipsa, corrupti sunt, corporis, labore tempore perferendis minus quo possimus illo obcaecati magnam repellat voluptas! Nisi, voluptas molestias!"
-            }
-          </p>
+          <h1 className="poster-title">{this.state.title}</h1>
+          <p className="poster-description">{this.state.description}</p>
         </div>
       </section>
     );
   }
 }
 
-const mapStateToProps = state => (state);
+const mapStateToProps = state => state;
 
-export default connect(mapStateToProps, null)(Poster);
+export default connect(
+  mapStateToProps,
+  null
+)(Poster);

340bd98c710d48f054a2e009af0dc79afd842d9a
dandybytes <dandybytes@gmx.com>
2018-12-02 05:07:56 +0200

    added conditional rendering of a Spinner in both the Poster and the MovieBoard

--- src/components/MovieBoard.js
+++ src/components/MovieBoard.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
+import Spinner from "./Spinner";
 import MovieCard from "./MovieCard";
 import { url_img_poster } from "../tools/url";
 import "./MovieBoard.css";
 
 const MovieBoard = props => (
   <div className="movie-board">
-    <h1 className="movie-board-headline">Movies:</h1>
-    <div className="movie-board-content">
-      {props.movies &&
-        props.movies.map(movie => (
-          <MovieCard key={movie.id} img={url_img_poster + movie.poster_path} />
-        ))}
-    </div>
+    {!props ? (
+      <Spinner />
+    ) : (
+      <React.Fragment>
+        <h1 className="movie-board-headline">Movies:</h1>
+        <div className="movie-board-content">
+          {props.movies &&
+            props.movies.map(movie => (
+              <MovieCard
+                key={movie.id}
+                img={url_img_poster + movie.poster_path}
+              />
+            ))}
+        </div>
+      </React.Fragment>
+    )}
   </div>
 );
 
--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
 import "./Poster.css";
+import Spinner from "./Spinner";
 
-const Poster = ({ image, title, description }) => (
-  <section className="poster">
-    <div className="spinner-large" />
-    <div
-      className="poster-image"
-      style={{
-        backgroundImage: `linear-gradient(var(--color-1), transparent 20%), url("${image}")`
-      }}
-    />
-    <div className="poster-text">
-      <h1 className="poster-title">{title}</h1>
-      <p className="poster-description">{description}</p>
-    </div>
-  </section>
-);
+const Poster = ({ image, title, description }) => {
+  return (
+    <section className="poster">
+      {!image ? (
+        <div className="poster-spinner">
+          <Spinner />
+        </div>
+      ) : (
+        <React.Fragment>
+          <div className="spinner-large" />
+          <div
+            className="poster-image"
+            style={{
+              backgroundImage: `linear-gradient(var(--color-1), transparent 20%), url("${image}")`
+            }}
+          />
+          <div className="poster-text">
+            <h1 className="poster-title">{title}</h1>
+            <p className="poster-description">{description}</p>
+          </div>
+        </React.Fragment>
+      )}
+    </section>
+  );
+};
 
 const mapStateToProps = state => ({ ...state.movies.poster });
 

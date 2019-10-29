8b895e8d276c477a0cc4271d460ad1d0069c24ca
dandybytes <dandybytes@gmx.com>
2019-10-29 20:50:57 +0200

    add link to Logo leading to the home page

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,15 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import "./Logo.css";
 
 const Logo = () => {
-  return (
-    <div className="logo">
-      <h1 className="logo-title">MovieFinder</h1>
-    </div>
-  );
+    return (
+        <div className="logo">
+            <Link className="logo-link" to="/">
+                <h1 className="logo-title">MovieFinder</h1>
+            </Link>
+        </div>
+    );
 };
 
 export default Logo;

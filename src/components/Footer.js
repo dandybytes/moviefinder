a3f6b3a50dd482d0997067a28a0ecb2e4f75cde5
dandybytes <dandybytes@gmx.com>
2019-10-29 21:01:03 +0200

    link MovieDB logo in the footer to the MovieDB info page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,31 +2,37 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className="footer-credits">
-        <img
-          className="moviedb-logo"
-          src="/moviedb-logo.png"
-          alt="powered by the MovieDB API"
-        />
-      </div>
-      <div className="footer-note">
-        <q>Television is chewing gum for the eyes.</q>
-      </div>
-      <div className="footer-social-media">
-        <a href="/">
-          <i className="fab fa-twitter-square" />
-        </a>
-        <a href="/">
-          <i className="fab fa-facebook-square" />
-        </a>
-        <a href="/">
-          <i className="fab fa-snapchat-square" />
-        </a>
-      </div>
-    </footer>
-  );
+    return (
+        <footer>
+            <div className="footer-credits">
+                <a
+                    href="https://www.themoviedb.org/about/logos-attribution"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img
+                        className="moviedb-logo"
+                        src="/moviedb-logo.png"
+                        alt="powered by the MovieDB API"
+                    />
+                </a>
+            </div>
+            <div className="footer-note">
+                <q>Television is chewing gum for the eyes.</q>
+            </div>
+            <div className="footer-social-media">
+                <a href="/">
+                    <i className="fab fa-twitter-square" />
+                </a>
+                <a href="/">
+                    <i className="fab fa-facebook-square" />
+                </a>
+                <a href="/">
+                    <i className="fab fa-snapchat-square" />
+                </a>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;

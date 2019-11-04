52f4d6b7e70205c1a778f03b426c39f47cacb338
dandybytes <dandybytes@gmx.com>
2019-11-04 11:25:26 +0200

    convert MovieDetailsPage to functional component

--- src/components/pages/MovieDetailsPage.js
+++ src/components/pages/MovieDetailsPage.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useEffect} from "react";
 import {connect} from "react-redux";
 import {fetchMovieDetails} from "../../tools/fetch";
 import {setMovieDetails} from "../../actions/actions-movie-details";
@@ -7,37 +7,26 @@ import MovieStatStripe from "../MovieStatStripe";
 import Spinner from "../common/Spinner";
 import "./MovieDetailsPage.css";
 
-class MovieDetailsPage extends Component {
-    state = {};
+const MovieDetailsPage = props => {
+    useEffect(() => {
+        fetchMovieDetails(props.match.params.movieId).then(result => props.setMovieDetails(result));
+    }, []);
 
-    componentDidMount() {
-        fetchMovieDetails(this.props.match.params.movieId).then(result =>
-            this.props.setMovieDetails(result)
-        );
-    }
+    return (
+        <div className="movie-details-page">
+            {/* {props.movieDetails.status_code && <h1>movie cannot be found</h1>} */}
+            {props.movieDetails.id ? (
+                <React.Fragment>
+                    <MovieDetailsCore movie={props.movieDetails} />
+                    <MovieStatStripe movie={props.movieDetails} />
+                </React.Fragment>
+            ) : props.movieDetails.status_code ? (
+                <h1 style={{color: "red"}}>movie cannot be found</h1>
+            ) : (
+                <Spinner />
+            )}
+        </div>
+    );
+};
 
-    render() {
-        console.log("movie details props");
-        console.log(this.props);
-        return (
-            <div className="movie-details-page">
-                {/* {this.props.movie.status_code && <h1>movie cannot be found</h1>} */}
-                {this.props.movie.id ? (
-                    <React.Fragment>
-                        <MovieDetailsCore movie={this.props.movie} />
-                        <MovieStatStripe movie={this.props.movie} />
-                    </React.Fragment>
-                ) : this.props.movie.status_code ? (
-                    <h1 style={{color: "red"}}>movie cannot be found</h1>
-                ) : (
-                    <Spinner />
-                )}
-            </div>
-        );
-    }
-}
-
-export default connect(
-    state => state,
-    {setMovieDetails}
-)(MovieDetailsPage);
+export default connect(state => state, {setMovieDetails})(MovieDetailsPage); //prettier-ignore

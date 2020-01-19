5e6bdb0d95093fde4c2cee4c71f4fd5100932174
dandybytes <dandybytes@gmx.com>
2020-01-19 20:10:08 +0200

    convert SearchBar to stateless functional component with hooks; correct whitespace trimming error on keyboard input

--- src/components/common/SearchBar.js
+++ src/components/common/SearchBar.js
@@ -1,42 +1,38 @@
-import React, {Component} from "react";
+import React, {useState, useRef} from "react";
 import {connect} from "react-redux";
-import {setCategory, setQuery} from "../../actions/actions-movie-list";
+import {setQuery} from "../../actions/actions-movie-list";
 import "./SearchBar.css";
 import MagnifyingGlassIcon from "./MagnifyingGlassIcon";
 
-class SearchBar extends Component {
-    state = {query: ""};
+const SearchBar = ({setQuery}) => {
+    const [userEnteredQuery, setUserEnteredQuery] = useState("");
+    const timeout = useRef();
 
-    timeout;
-
-    handleSearchInput = event => {
-        const query = event.target.value.trim();
-        this.setState({query});
-        clearTimeout(this.timeout);
-        this.timeout = setTimeout(() => this.props.setQuery(this.state.query), 700);
+    const handleSearchInput = event => {
+        setUserEnteredQuery(event.target.value);
+        clearTimeout(timeout.current);
+        timeout.current = setTimeout(() => setQuery(userEnteredQuery), 500);
     };
 
-    handleSubmit = event => {
+    const handleSubmit = event => {
         event.preventDefault();
-        this.props.setQuery(this.state.query);
+        setQuery(userEnteredQuery.trim());
     };
 
-    render() {
-        return (
-            <form className="searchbar-form" onSubmit={this.handleSubmit}>
-                <label className="searchbar-label">
-                    <input
-                        type="search"
-                        className="searchbar-input"
-                        placeholder="movie title..."
-                        value={this.state.query}
-                        onChange={this.handleSearchInput}
-                    />
-                    <MagnifyingGlassIcon />
-                </label>
-            </form>
-        );
-    }
-}
+    return (
+        <form className="searchbar-form" onSubmit={handleSubmit}>
+            <label className="searchbar-label">
+                <input
+                    type="search"
+                    className="searchbar-input"
+                    placeholder="movie title..."
+                    value={userEnteredQuery}
+                    onChange={handleSearchInput}
+                />
+                <MagnifyingGlassIcon />
+            </label>
+        </form>
+    );
+};
 
-export default connect(null, {setCategory, setQuery})(SearchBar); //prettier-ignore
+export default connect(null, {setQuery})(SearchBar);

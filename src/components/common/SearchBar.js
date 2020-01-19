d355c74a60be5b15cd92a1f13e54590be02d01f3
dandybytes <dandybytes@gmx.com>
2020-01-19 23:19:14 +0200

    refactor SearchBar: 1 move query input debouncing to useEffect hook; 2 focus search input on component render

--- src/components/common/SearchBar.js
+++ src/components/common/SearchBar.js
@@ -1,23 +1,33 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import {connect} from "react-redux";
 import {setQuery} from "../../actions/actions-movie-list";
 import "./SearchBar.css";
 import MagnifyingGlassIcon from "./MagnifyingGlassIcon";
 
-const SearchBar = ({setQuery}) => {
-    const [userEnteredQuery, setUserEnteredQuery] = useState("");
-    const timeout = useRef();
+const SearchBar = ({storeQuery, setStoreQuery}) => {
+    const [inputQuery, setInputQuery] = useState("");
+    const timeoutRef = useRef(null);
+    const searchInputRef = useRef(null);
 
-    const handleSearchInput = event => {
-        setUserEnteredQuery(event.target.value);
-        clearTimeout(timeout.current);
-        timeout.current = setTimeout(() => setQuery(userEnteredQuery), 500);
-    };
+    const handleSearchInput = event => setInputQuery(event.target.value);
 
     const handleSubmit = event => {
         event.preventDefault();
-        setQuery(userEnteredQuery.trim());
+        setStoreQuery(inputQuery.trim());
     };
+
+    useEffect(() => {
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
+        timeoutRef.current = setTimeout(() => setStoreQuery(inputQuery), 350);
+
+        return () => clearTimeout(timeoutRef.current);
+    }, [inputQuery, setStoreQuery]);
+
+    useEffect(() => {
+        searchInputRef.current.focus();
+        if (inputQuery === "" && storeQuery !== "") setInputQuery(storeQuery);
+        // eslint-disable-next-line
+    }, []);
 
     return (
         <form className="searchbar-form" onSubmit={handleSubmit}>
@@ -26,8 +36,9 @@ const SearchBar = ({setQuery}) => {
                     type="search"
                     className="searchbar-input"
                     placeholder="movie title..."
-                    value={userEnteredQuery}
+                    value={inputQuery}
                     onChange={handleSearchInput}
+                    ref={searchInputRef}
                 />
                 <MagnifyingGlassIcon />
             </label>
@@ -35,4 +46,6 @@ const SearchBar = ({setQuery}) => {
     );
 };
 
-export default connect(null, {setQuery})(SearchBar);
+const mapStateToProps = state => ({storeQuery: state.movies.query});
+
+export default connect(mapStateToProps, {setStoreQuery: setQuery})(SearchBar);
